28581c2e9f5877c4dae33b3ec336afdddbd35520
Gregor Weber <gregorwbr@gmail.com>
2014-07-10 23:25:32 +0200

    Feature: Differentiate between different kind of listeners (single, multi), so it can decide whether to return eventName on event Enhancements: More functions, moar clarity

--- src/lib/listenable.js
+++ src/lib/listenable.js
@@ -16,25 +16,30 @@ define(function() {
 		on: function(eventName, callback) {
 			if (typeof eventName == 'string') {
 				var events = eventName.split(' ');
-
-				if (events.length == 1) {
-					if (!this._listeners[eventName]) this._listeners[eventName] = [];
-					this._listeners[eventName].push(callback);
-				} else for (var i = 0; i < events.length; i++) this.on(events[i], callback);
-			} else {
-				var eventMap = eventName;
-				events = Object.keys(eventMap);
-				for (var j = 0; j < events.length; j++) {
-					var event = events[j],
-						callbacks = eventMap[event];
-
-					if (typeof callbacks != 'object') callbacks = [callbacks];
-
-					for (var k = 0; k < callbacks.length; k++) if (callbacks[k]) this.on(event, callbacks[k]);
+				if (events.length == 1) this._addListener(eventName, callback, 'single');
+				else for (var i = 0; i < events.length; i++) {
+					this._addListener(events[i], callback, 'multi');
 				}
-			}
+			} else this._onMap(eventName);
 
 			return this.obj;
+		},
+		_addListener: function(eventName, callback, type) {
+			if (!this._listeners[eventName]) this._listeners[eventName] = [];
+			this._listeners[eventName].push({callback: callback, type: type});
+		},
+		_onMap: function(eventMap) {
+			var events = Object.keys(eventMap);
+			for (var j = 0; j < events.length; j++) {
+				var event = events[j],
+					callbacks = eventMap[event];
+
+				if (typeof callbacks != 'object') callbacks = [callbacks];
+
+				for (var k = 0; k < callbacks.length; k++) if (callbacks[k]) {
+					this._addListener(event, callbacks[k], 'single');
+				}
+			}
 		},
 		trigger: function(eventName) {
 			if (eventName == 'all') throw 'Triggering all events is an anti-pattern because \n';
@@ -42,14 +47,18 @@ define(function() {
 				allListeners	=	this._listeners['all'];
 
 			if (eventListeners) for (var i = 0; i < eventListeners.length; i++) {
-				eventListeners[i].apply(this.obj, Array.prototype.slice.call(arguments, 1));
+				var listener = eventListeners[i];
+				this._triggerCallback(listener.callback, arguments, listener.type == 'multi');
 			}
 
-			if (allListeners) for (var i = 0; i < allListeners.length; i++) {
-				allListeners[i].apply(this.obj, arguments);
+			if (allListeners) for (var j = 0; j < allListeners.length; j++) {
+				this._triggerCallback(allListeners[j].callback, arguments, true);
 			}
 
 			return this.obj;
+		},
+		_triggerCallback: function(callback, args, withEventName) {
+			callback.apply(this.obj, Array.prototype.slice.call(args, withEventName ? 0 : 1));
 		}
 	};
 
